c49b775da6082b9fdf011d596145cfed78d314b8
Py-Dude <62688535+Py-Dude@users.noreply.github.com>
2024-01-15 00:01:41 +0530

    Graph

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 import Intro from "./Components/Intro/Intro.js";
 // import styles of this component
 import styles from "./App.module.css";
-import { createTheme, ThemeProvider, CssBaseline } from '@mui/material/styles';
-import { LineChart } from '@mui/x-charts/LineChart';
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material/styles";
+import { LineChart } from "@mui/x-charts/LineChart";
 
 // import other components to use
 import MasonryLayout from "./Components/MasonryLayout/MasonryLayout";
@@ -17,8 +17,13 @@ import images from "./Jsons/Images.json";
 
 // App component
 const App = () => {
-  const data = [1, 2, 3, 5, 8, 10];
-const lineData = [2, 5.5, 2, 8.5, 1.5, 5];
+  const data = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+  const lineData = [
+    97, 93, 89, 94, 85, 82, 78.7, 77.4, 76.5, 92, 79.6, 93.5, 59, 77, 76, 82,
+    78, 79, 80, 100,
+  ];
 
   // dropdown items
   const ddItems = [
@@ -81,38 +86,54 @@ const lineData = [2, 5.5, 2, 8.5, 1.5, 5];
           </div>
           <MasonryLayout images={categoryImage} />
           <LineChart
-           id="myLineChart"
- xAxis={[{ data: data }]}
- series={[{ data: lineData }]}
- width={500}
- height={300}
- sx={{
-  "& .MuiChartsAxis-left .MuiChartsAxis-tickLabel": {
-    fontSize: "20px",
-    fill: "#ff0000", // Change the color of y-axis labels
-  },
-  "& .MuiChartsAxis-bottom .MuiChartsAxis-tickLabel": {
-    fontSize: "20px", // Change the font size of x-axis labels
-    fill: "#ff0000", // Change the color of y-axis labels
-
-  },
-  "& .MuiChartsAxis-bottom .MuiChartsAxis-line": {
-    stroke: "hotpink", // Change the color
-    strokeWidth: 2, // Change the width
-  },
-  "& .MuiChartsAxis-left .MuiChartsAxis-line": {
-    stroke: "hotpink", // Change the color
-    strokeWidth: 2, // Change the width
-    
-  },
-}}
-/>
-
+            id="myLineChart"
+            xAxis={[
+              {
+                data: data,
+                tickNumber: 10,
+                label: "Percentage",
+                labelStyle: {
+                  fontSize: "20px", // Set the font size of the axis labels
+                  fill: "#ffb9b9", // Set the color of the axis labels
+                },
+              },
+            ]}
+            series={[{ data: lineData }]}
+            yAxis={[
+              {
+                tickNumber: 10,
+                label: "Standard",
+                labelStyle: {
+                  fontSize: "20px", // Set the font size of the axis labels
+                  fill: "#ffb9b9", // Set the color of the axis labels
+                  transform: "translateX(-10px)",
+                },
+              },
+            ]}
+            width={1500}
+            height={800}
+            sx={{
+              "& .MuiChartsAxis-left .MuiChartsAxis-tickLabel": {
+                fontSize: "500px",
+                fill: "#ffb9b9", // Change the color of y-axis labels
+              },
+              "& .MuiChartsAxis-bottom .MuiChartsAxis-tickLabel": {
+                fontSize: "500px", // Change the font size of x-axis labels
+                fill: "#ffb9b9", // Change the color of y-axis labels
+              },
+              "& .MuiChartsAxis-bottom .MuiChartsAxis-line": {
+                stroke: "hotpink", // Change the color
+                strokeWidth: 4, // Change the width
+              },
+              "& .MuiChartsAxis-left .MuiChartsAxis-line": {
+                stroke: "hotpink", // Change the color
+                strokeWidth: 4, // Change the width
+              },
+            }}
+          />
         </ContainerCard>
       </div>
-      <div>
-
-      </div>
+      <div></div>
     </>
   );
 };
